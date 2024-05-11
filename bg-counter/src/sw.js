d4e495ff00d71156227955e0f835ca2b2abd1c74
Fabian Stroh <fabian.stroh@andrena.de>
2024-05-11 22:08:40 +0200

    make pwa

---
+++ bg-counter/src/sw.js
@@ -0,0 +1,49 @@
+const GHPATH = '/bg-counter/';
+const APP_PREFIX = 'bgc_';
+const VERSION = 'version_000';
+
+
+const CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener('fetch', function (e) {
+    console.log('Fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('Responding with cache : ' + e.request.url);
+                return request
+            } else {
+                console.log('File is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+        })
+    )
+})
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) =>
+            fetch('/assets.json')
+                .then((response) => response.json())
+                .then((assets) =>
+                    cache.addAll(assets.map((asset) => GHPATH + asset))
+                )
+        )
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            const cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+            cacheWhitelist.push(CACHE_NAME);
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('Deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i])
+                }
+            }))
+        })
+    )
+})
--- bg-counter/vite.config.ts
+++ bg-counter/vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -8,6 +9,21 @@ export default defineConfig({
   base: '',
   plugins: [
     vue(),
+    {
+      name: 'list-assets',
+      apply: 'build',
+      generateBundle(_, output) {
+        const assets = Object.values(output)
+            // .filter(item => item.type === 'asset')
+            .map(asset => asset.fileName)
+
+        this.emitFile({
+          type: 'asset',
+          fileName: 'assets.json',
+          source: JSON.stringify(assets, null, 2)
+        })
+      }
+    }
   ],
   resolve: {
     alias: {
