44c99e4ddca66c5688e929415f7d1ef215e83d61
Fabian Stroh <fabian.stroh@andrena.de>
2024-08-21 08:13:57 +0200

    update dependencies

--- bg-counter/public/sw.js
+++ bg-counter/public/sw.js
@@ -1,6 +1,6 @@
 const GHPATH = '/bg-counter/';
 const APP_PREFIX = 'bgc_';
-const VERSION = 'version_007';
+const VERSION = 'version_008';
 const FILES = ['', 'favicon.ico', 'android-chrome-192x192.png']
 
 
